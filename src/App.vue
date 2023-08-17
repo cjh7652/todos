57227c8ef3af5c67509613bce963cfbdf055372a
jeehyun <cjh7652@hanamail.net>
2023-08-17 14:37:02 +0900

    deploy

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,11 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+
 
 nav {
   padding: 30px;
+  border-bottom: 1px solid #ddd;
 
   a {
     font-weight: bold;
---
+++ src/components/TodoList.vue
@@ -0,0 +1,39 @@
+<template lang="ko">
+    <div class="card" v-for="(todo, index) in todos" :key="todo.id">
+        <div class="card-body">
+            <div class="checkbox-wrapper-11">
+                <input :id="todo.id" type="checkbox" name="r"  :checked="todo.completed" @change="toggleTodo(index)">
+                <label :for="todo.id">{{todo.subject}}</label>
+            </div>
+            <div>
+                <button class="btn-delete" @click="deleteTodo(index)">삭제</button>
+            </div>
+        </div>
+     </div>
+</template>
+<script>
+export default {
+    props: {
+        todos: {
+            type: Array,
+            required:true
+        }
+    },
+    emits:['delete-todo', 'toggle-todo'],
+    setup(props, {emit}){
+        const deleteTodo = (index) =>{
+           emit('delete-todo', index)
+        }
+        const toggleTodo= (index) =>{
+            emit('toggle-todo', index)
+        }
+        return{
+            deleteTodo,
+            toggleTodo
+        }
+    }
+}
+</script>
+<style lang="scss">
+    
+</style>
